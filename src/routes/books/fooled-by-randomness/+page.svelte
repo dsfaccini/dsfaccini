5447b4ad45c3947ff991966f7eee0e54b9bea970
David <david.faccini@rwth-aachen.de>
2023-01-15 16:41:23 +0100

    fooled by randomness

---
+++ src/routes/books/fooled-by-randomness/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	const preficeQuote = `The non-profit journalist is another intellectual breed.
+    Casually, the quality of the discussion correlates inversely with the luxury of the studios.
+    [...]
+    I elected to keep the style of this book as 'idiosyncratic', as it was on the first edition.
+    Homo sum, good and bad. I am fallible, and see no reason to hide my minor flaws if they are part of my personality.
+    No more do I feel the need to wear a wig when I have my picture taken, or borrow someone else's nose.
+    Almost all the book editors that read the draft recommended changes at the sentence level, to make my 'style better'
+    and in the structure of the text, in the organization of chapters.
+    I ignored almost all of them and found out that none of the readers thought them necessary.
+    As a matter of fact, I find that injecting the personality of the author, imperfections included, enlivens the text.
+
+    Does the book industry suffer from the classical "expert problem" with a buildup of rules of thumb that do not have empirical validity?
+    More than a hundred thousand readers later, I am discovering that books are not written for book editors.`;
+</script>
+
+<blockquote>{preficeQuote}</blockquote>
+<p>I don't personally like Taleb, nor agree with a lot of his opinions</p>
+<p>
+	Nonetheless I do enjoy his books and the way they introduce people to a sort of 'statistical
+	reasoning' <br />
+	Regardless of Your opinions, they will certainly make You consider things from a different perspective
+	<br />
+	I chose this quote as it talks about what happens to systems that live long enough too meet their disruptors
+</p>
+<p>
+	As an example and to continue the topic of journalism, gone are the days where networks would
+	arrange panels and bring in fighters from all corners of the ring to present their take on a
+	current topic <br />
+	Nowaday's it's a surprise whenever a person shows up that You haven't seen before <br />
+	They have inhouse 'experts', like their friend Mike with whom they had dinner last night <br />
+	Moreover, You change the channel and hear the same thing being said by a different person <br />
+	Long form podcasts are devouring media, and hard as networks try to keep up, they can't <br />
+</p>
+<p>
+	The value of legacy networks lies in their reputation, which is why they've built standardized
+	systems <br />
+	On what topics they discuss, how they discuss them, whom they invite, what words they use, how much
+	time they devote to a topic before switching contexts
+</p>
+
+<!-- TODO talk about
+    reputation and the mask that we put on ourselves
+    gatekeeping (technical terms that become too much, keeping people out with new ideas)
+    the fear of invalidating Your life's work
+    the fear of cannibalization / destructive creation
+-->
